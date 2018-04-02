7d37589bd7c441dc0ec32ea8fb170226fe2cc2b3
Kiran Kumar Bali <kirankumarbali@users.noreply.github.com>
2018-04-02 15:23:36 +0530

    New updates
    New updates

--- NXT/js/jquery.slitslider.js
+++ NXT/js/jquery.slitslider.js
@@ -68,7 +68,7 @@
 
 	$.Slitslider.defaults = {
 		// transitions speed
-		speed : 800,
+		speed : 1500,
 		// if true the item's slices will also animate the opacity value
 		optOpacity : false,
 		// amount (%) to translate both slices - adjust as necessary
